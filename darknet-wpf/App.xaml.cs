ead05385a4d8fa2a65ec862c8c060290dc3123cd
Ben Hutchison <ben@aldaviva.com>
2021-03-19 07:33:29 -0700

    Working somehow

---
+++ darknet-wpf/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace darknet_wpf {
+
+    public partial class App {
+
+        private void App_OnStartup(object sender, StartupEventArgs e) {
+            DarkMode.setDarkModeAllowedForProcess(true); //you can tell if this worked because the title bar's context menu will be dark
+
+            var window = new MainWindow();
+            window.Show();
+            DarkMode.setDarkModeAllowedForWindow(window, true); //do this before or after showing?
+        }
+
+    }
+
+}
---
+++ darknet-wpf/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace darknet_wpf {
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: Window {
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+    }
+
+}
